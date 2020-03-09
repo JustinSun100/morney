ffc0ce5efb6619735c4b8520e2686e77455e731a
justinsun <sun_heqing@163.com>
2020-03-09 14:19:32 +0800

    money的html和css写好了

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
     <nav>
-        <router-link to="money" class="item">
+        <router-link to="money" class="item" active-class="selected">
             <icon name="money"/>
             记账
         </router-link>
-        <router-link to="labels" class="item">
+        <router-link to="labels" class="item" active-class="selected">
             <icon name="label"/>
             标签
         </router-link>
-        <router-link to="statistics" class="item">
+        <router-link to="statistics" class="item" active-class="selected">
             <icon name="statistics"/>
             统计
         </router-link>
@@ -27,8 +27,8 @@
 
 <style lang="scss" scoped>
     nav {
+        @extend %outerShadow !optional;
         display: flex;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
         flex-direction: row;
         font-size: 12px;
         > .item {
