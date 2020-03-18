9edd2d62d82a5e6f7ee1da948453e297b9ef4c33
justinsun <sun_heqing@163.com>
2020-03-18 20:14:21 +0800

    将model融合进store

---
+++ src/store/index2.ts
@@ -0,0 +1,14 @@
+
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
+
+
+const store = {
+...recordStore,
+    //recordList store
+...tagStore
+
+//tag store
+
+};
+export default store
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -17,16 +17,15 @@
     import Types from '@/components/Money/Types.vue';
     import FormItem from '@/components/Money/FormItem.vue';
     import {Component, Watch} from 'vue-property-decorator';
-    import recordListModel from '@/models/recordListModel';
-    import tagListModel from '@/models/tagListModel';
+    import store from '@/store/index2';
 
 
     @Component(
         {components: {Tags, FormItem, Types, NumberPad}}
     )
     export default class Money extends Vue {
-        recordList = window.recordList;
-        tags = window.tagList;
+        recordList = store.recordList;
+        tags = store.tagList;
         record: RecordItem = {
             tags: [], notes: '', type: '-', amount: 0
         };
@@ -45,7 +44,7 @@
         }
 
         saveRecord() {
-            window.createRecord(this.record);
+           store.createRecord(this.record);
         }
 
     }
