3985298fc0199e45a3b841c9443b8de704217a2a
justinsun <sun_heqing@163.com>
2020-03-24 14:22:53 +0800

    封装Tabs组件、constants目录

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,8 +1,7 @@
 <template>
     <Layout class-prefix="layout">
-        {{record}}
         <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
-        <Types :value.sync="record.type"/>
+        <tabs :data-source="typeList" :value.sync="record.type"/>
         <div class="notes">
             <FormItem filename="备注" placeholder="这里输入备注" @update:value="onUpdateNotes"/>
         </div>
@@ -14,13 +13,14 @@
     import Vue from 'vue';
     import NumberPad from '@/components/Money/NumberPad.vue';
     import Tags from '@/components/Money/Tags.vue';
-    import Types from '@/components/Money/Types.vue';
     import FormItem from '@/components/Money/FormItem.vue';
     import {Component, Watch} from 'vue-property-decorator';
+    import typeList from '@/constants/typeList';
+    import Tabs from '@/components/Tabs.vue';
 
 
     @Component({
-            components: {Tags, FormItem, Types, NumberPad}
+            components: {Tabs, Tags, FormItem,  NumberPad}
         }
     )
     export default class Money extends Vue {
@@ -28,6 +28,7 @@
             return this.$store.state.recordList;
         }
 
+        typeList=typeList
         tags = [];//store.tagList;
         record: RecordItem = {
             tags: [], notes: '', type: '-', amount: 0
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,12 +1,43 @@
 <template>
     <Layout>
-        statistics.vue
+        <Tabs class-prefix="type" :data-source="typeList" :value.sync="type"/>
+        <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
+        <div>
+            type:{{type}}<br/>
+            interval:{{interval}}
+        </div>
     </Layout>
+
 </template>
 
+<style scoped lang="scss">
+    ::v-deep .type-tabs-item {
+        background: white;
+
+        &.selected {
+            background: #c4c4c4;
+
+            &::after {
+                display: none;
+            }
+        }
+    }
+</style>
 <script lang="ts">
-    export default {
-        name: 'Statistics'
-    };
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+    import Tabs from '@/components/Tabs.vue';
+    import intervalList from '@/constants/intervalList';
+    import typeList from '@/constants/typeList';
+
+    @Component({
+        components: {Tabs}
+    })
+    export default class Statistics extends Vue {
+        interval = 'day';
+        type = '-';
+        intervalList = intervalList;
+        typeList = typeList;
+    }
 </script>
 
